087254dd77fefcd3f498e81a05471a15d3cf1f88
aydie <aydieprodstudio@gmail.com>
2025-10-05 19:35:53 +0530

    Added Inngest AgentKit and model selection.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 const Page = () => {
   const trpc = useTRPC();
+  const [value, setValue] = useState("");
   const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess: () => {
       toast.success("Background Job Started")
@@ -15,7 +18,8 @@ const Page = () => {
   
   return (
     <div className="p-4 max-w-7xl mx-auto">
-        <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ text: "Aydie"})}>
+        <Input value={value} onChange={(e) => setValue(e.target.value)}/>
+        <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ value: value})}>
           Invoke Background Job
         </Button>
     </div>
