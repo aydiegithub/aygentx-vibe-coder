e7b68c8d55a97eb077137b3cdec8c73fcade788b
aydie <aydieprodstudio@gmail.com>
2025-10-16 11:35:27 +0530

    Integrated E2B Sandbox and Dockerfile

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,20 @@
 import { inngest } from "./client";
 import { Agent, gemini, createAgent} from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      if (!process.env.E2B_TEMPLATE_NAME) {
+      throw new Error("E2B_TEMPLATE_NAME is not set in environment variables");
+    }
+      const sandbox = await Sandbox.create(process.env.E2B_TEMPLATE_NAME);
+      return sandbox.sandboxId;
+    });
+
     const codeAgent = createAgent({
       name: "codeAgent",
       system: `You are an expert Next.js and React developer. 
@@ -20,6 +30,12 @@ export const helloWorld = inngest.createFunction(
     );
     console.log(output);
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   },
 );  
