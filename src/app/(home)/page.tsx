b58db40387f81f5df9882ed82979cbc917c9bf12
aydie <aydieprodstudio@gmail.com>
2025-11-04 14:27:25 +0530

    Added OG Images

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,37 @@ import { ProjectForm } from "@/modules/home/ui/components/project-form";
 import { ProjectsList } from "@/modules/home/ui/components/projects-list";
 import Image from "next/image";
 
+export const metadata = {
+  title: "AygentX Vibe Coder | Build with AI",
+  description:
+    "Experience the future of coding — build full-stack apps, websites, and AI projects instantly by chatting with AygentX Vibe Coder. No setup. No limits. Just pure creation.",
+  openGraph: {
+    title: "AygentX Vibe Coder | Build with AI",
+    description:
+      "Experience the future of coding — build full-stack apps, websites, and AI projects instantly by chatting with AygentX Vibe Coder. No setup. No limits. Just pure creation.",
+    url: "https://aydie.in/aygentx/og-image.png",
+    siteName: "AygentX Vibe Coder",
+    images: [
+      {
+        url: "https://aydie.in/aygentx/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "AygentX Vibe Coder",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "AygentX Vibe Coder | Build with AI",
+    description:
+      "Experience the future of coding — build full-stack apps, websites, and AI projects instantly by chatting with AygentX Vibe Coder. No setup. No limits. Just pure creation.",
+    images: ["https://aydie.in/aygentx/og-image.png"],
+  },
+};
+
+
 const Page = () => {
   return (
     <div className="flex flex-col max-w-5xl mx-auto w-full">
@@ -30,4 +61,4 @@ const Page = () => {
   );
 };
 
-export default Page;  
+export default Page;
